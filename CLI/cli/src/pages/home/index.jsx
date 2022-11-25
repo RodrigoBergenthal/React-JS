e47516ccb708731f85e42724c2bad6c4264e3523
RodrigoBergenthal <rodrigo_bergenthal@live.com>
2022-11-25 16:46:23 -0300

    atualizando

--- CLI/cli/src/pages/home/index.jsx
+++ CLI/cli/src/pages/home/index.jsx
@@ -1,8 +1,35 @@
 import { Link } from 'react-router-dom';
+
+import { Button } from '../../components/Button';
+
+import { Header } from '../../components/Header';
+
+import BannerImage from '../../assets/banner.png';
+import { Container, TextContent, Title, TitleHighLight } from './style';
+
 const Home = () => {
     return (<>
-        <h1> Home </h1>
-        <Link to="/Login"> Fazer login </Link>
+        <Header />
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighLight>
+                        Implemente
+                        <br />
+                    </TitleHighLight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, 
+                    evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title="Comecar agora" variant="secondary" onclick={()=> null}/>
+
+            </div>
+            <div>
+                
+            </div>
+        </Container>
+        <img src={BannerImage} alt="Imagem principal" />
         </>)
 }
 export { Home }
